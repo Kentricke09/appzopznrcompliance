7166efc0462d75c16305aca95a635ff252279845
Kentricke09 <fazlic.kenan@gmail.com>
2026-08-23 15:51:56 +0200

    Add files via upload

--- reset_db.py
+++ reset_db.py
@@ -1,15 +1,23 @@
 import sqlite3
 
-conn = sqlite3.connect("baza.db")
+# Ime baze mora biti ono koje koristiš u aplikaciji
+conn = sqlite3.connect("znr_baza.db")
 cursor = conn.cursor()
 
-# Brišemo podatke iz svih tabela
-cursor.execute("DELETE FROM datumi")
-cursor.execute("DELETE FROM lokacije")
-cursor.execute("DELETE FROM kompanije")
+# Brišemo podatke iz svih tabela prema trenutnoj šemi
+cursor.execute("DELETE FROM masine")
+cursor.execute("DELETE FROM obuke")
+cursor.execute("DELETE FROM ljekarski")
+cursor.execute("DELETE FROM rizicna_mjesta")
+cursor.execute("DELETE FROM opasne_materije")
+cursor.execute("DELETE FROM lzo")
+cursor.execute("DELETE FROM posude_pod_pritiskom")
+cursor.execute("DELETE FROM povrede_na_radu")
+
+# Ako želiš da očistiš i podatke firme iz tabele korisnika (da vratiš na prazno):
+cursor.execute("UPDATE korisnici SET podaci_firme = '{}'")
 
 conn.commit()
 conn.close()
 
-uspjeh = "Baza je uspješno očišćena i ispražnjena!"
-print(uspjeh)
+print("Baza 'znr_baza.db' je uspješno očišćena i ispražnjena!")
